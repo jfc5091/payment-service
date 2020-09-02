eb9d0c02d787b23124ccbd62b3ed4df927997d96
Joe Calamari <jfc5091@rit.edu>
2020-09-02 19:16:58 -0400

    added an order id for each payment that is processed

--- src/main/java/com/example/service/PaymentService.java
+++ src/main/java/com/example/service/PaymentService.java
@@ -5,6 +5,7 @@ import com.example.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
 import java.util.UUID;
 
 @Service //necessary for spring to maintain relationships
@@ -14,7 +15,12 @@ public class PaymentService {
     private PaymentRepository repository;
 
     public Payment doPayment(Payment payment) {
+        payment.setPaymentStatus(paymentProcessing());
         payment.setTransactionId(UUID.randomUUID().toString()); //just setting transaction id to random number
         return repository.save(payment);
+    }
+
+    public String paymentProcessing() {
+        return new Random().nextBoolean() ? "success" : "false";
     }
 }
