4b3ecd81991e33183311f0cc03aaa596af488055
Joe Calamari <jfc5091@rit.edu>
2020-09-02 00:00:36 -0400

    initial commit

---
+++ src/main/java/com/example/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import com.example.entity.Payment;
+import com.example.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService service;
+
+    @PostMapping("/doPayment")
+    public Payment doPayment(@RequestBody Payment payment) {
+        return service.doPayment(payment);
+    }
+}
---
+++ src/main/java/com/example/service/PaymentService.java
@@ -0,0 +1,20 @@
+package com.example.service;
+
+import com.example.entity.Payment;
+import com.example.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private PaymentRepository repository;
+
+    public Payment doPayment(Payment payment) {
+        payment.setTransactionId(UUID.randomUUID().toString());
+        return repository.save(payment);
+    }
+}
